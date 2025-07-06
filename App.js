e1345544dfe78942585ec285e0d00d89fcebb062
icevalentine <100855502+icevalentine@users.noreply.github.com>
2025-07-06 21:13:58 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import CanvasBoard from "./CanvasBoard";
+import logo from './logo.png';
 
 function App() {
   return (
+    
     <div style={{ padding: "1rem" }}>
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      </header>
       <CanvasBoard />
     </div>
   );
